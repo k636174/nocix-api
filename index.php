389d125cfaa5b1c45f9a739a7748dfa4898ac2a6
Shinji Kaneko <shinji@k636174.net>
2019-02-11 19:53:09 +0900

    change backgroundColor and fontColor

--- index.php
+++ index.php
@@ -9,27 +9,46 @@ if($env_file){
 }
 fclose($env_file);
 
+$result = curl_get('https://my.nocix.net/api/list-services/');
 
-$userAuth=getenv("USER").':'.getenv("AUTH_TOKEN");
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://my.nocix.net/api/list-services/');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-curl_setopt($ch, CURLOPT_USERPWD, $userAuth);
-$data = curl_exec($ch);
-curl_close($ch);
-$result = json_decode($data);
-
-
+?>
+<body style="background-color:black;color:orange">
+<?php
 foreach($result as $val){
     echo '<h1>契約ID:'.$val->id.'</h1>';
     echo '<table border="1">';
     foreach($val as $key => $val2){
         echo '<tr>';
         echo '<td>'.$key.'</td>';
-        echo '<td>'.$val2.'</td>';
+        echo '<td>' .$val2.'</td>';
         echo '</tr>';
     }
     echo '</table>';
 }
+
+function curl_get($url=null){
+
+    if($url){
+        $userAuth=getenv("USER").':'.getenv("AUTH_TOKEN");
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, $userAuth);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($data);
+    }else{
+        return false;
+    }
+}
+
+function pr($data){
+    echo "<pre>";
+    var_dump($data);
+    echo "</pre>";
+}
+
+?>
+</body>
